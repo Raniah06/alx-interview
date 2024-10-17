7077a53b0e5b909267063d17abf85c45ebd45343
Raniah06 <raniafadil06@gmail.com>
2024-10-17 19:20:33 +0200

    m

---
+++ 0x02-minimum_operation/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def minOperations(n):
+    # If n is 1 or less, no operations are needed or possible
+    if n <= 1:
+        return 0
+
+    operations = 0  # This will store the total number of operations needed
+    divisor = 2     # Start with the smallest possible divisor (2)
+    
+    # Keep dividing n by the current divisor as long as it's divisible
+    while n > 1:
+        while n % divisor == 0:   # While n is divisible by 'divisor'
+            operations += divisor  # Add the divisor to the count of operations
+            n //= divisor          # Reduce n by dividing it by the divisor
+        divisor += 1               # Move to the next divisor
+    
+    return operations
